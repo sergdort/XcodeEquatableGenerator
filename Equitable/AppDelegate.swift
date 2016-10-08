bcf615c1ee5642584681d20d817781557b7db78c
sergdort <sergdort@gmail.com>
2016-10-08 20:20:50 +0100

    Initial Commit

---
+++ Equitable/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Equitable
+//
+//  Created by sergdort on 08/10/2016.
+//  Copyright © 2016 sergdort. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Equitable/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Equitable
+//
+//  Created by sergdort on 08/10/2016.
+//  Copyright © 2016 sergdort. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
